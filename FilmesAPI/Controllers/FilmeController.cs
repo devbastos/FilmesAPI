3489dcb3b83754a3fe02effcf5f8fb6f8016723a
devbastos <86385468+devbastos@users.noreply.github.com>
2023-04-14 16:57:42 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,45 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            filme.Id = Id++;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id")]
+        public Filme RecuperaFilmePorId(int id)
+        {
+            foreach (Filme filme in filmes)
+            {
+                if (filme.Id == id)
+                {
+                    return filme;
+                }
+            }
+            return null;
+        }
+
+
+
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve estar entre 1 e 600 minutos")]
+
+        public int Duracao { get; set; }
+
+    }
+}
